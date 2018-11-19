6c7c34b68917babf867e54111f652fec7234315d
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-11-19 14:07:18 -0700

    Implemented the IsSolved method
    Before we can test it, we need to create a class that implements the cell solution algorithm, then call that.

--- HW4-OO/HW4-OO/CellSolutionAlgorithm.cs
+++ HW4-OO/HW4-OO/CellSolutionAlgorithm.cs
@@ -8,8 +8,21 @@ namespace HW4_OO
 {
     public abstract class CellSolutionAlgorithm
     {
-        public abstract bool SolveCell(SudokuPuzzle _Puzzle, int _Row, int _Column);
-        public bool IsSolved(SudokuPuzzle _Puzzle);
-        public bool Solve(SudokuPuzzle _Puzzle);
+        //public abstract bool SolveCell(SudokuPuzzle _Puzzle, int _Row, int _Column);
+        public bool IsSolved(SudokuPuzzle _Puzzle)
+        {
+            for (int i = 0; i < _Puzzle.Rows; i++)
+            {
+                for (int j = 0; j < _Puzzle.Rows; j++)
+                {
+                    if (_Puzzle.Cells[i,j].Value == '-')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        //public bool Solve(SudokuPuzzle _Puzzle);
     }
 }
---
+++ HW4-OO/UnitTestProject1/CellSolutionAlgorithmTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW4_OO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CellSolutionAlgorithmTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<char> Chars = new List<char> { '1', '2', '3', '4' };
+            string[] puzzleItems = "12-1\n---2\n3-2-\n-4-3".Split();
+            SudokuPuzzle myPuzzle = new SudokuPuzzle(Chars, 4, puzzleItems);
+            //CellSolutionAlgorithm solver = new  implemented version of this CellSolutionAlgorithm();
+  
+        }
+    }
+}
