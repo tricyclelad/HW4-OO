81a08768ee1b3341a40bcfcb610272adfdab1caa
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-11-19 13:57:21 -0700

    Created the abstract base class for my solution algorithm

---
+++ HW4-OO/HW4-OO/CellSolutionAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_OO
+{
+    public abstract class CellSolutionAlgorithm
+    {
+        public abstract bool SolveCell(SudokuPuzzle _Puzzle, int _Row, int _Column);
+        public bool IsSolved(SudokuPuzzle _Puzzle);
+        public bool Solve(SudokuPuzzle _Puzzle);
+    }
+}
