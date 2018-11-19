16760b90668ee7457240ed552e71911c7e8cf2aa
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-11-19 10:55:26 -0700

    Started Reader unit test
    Stable. currently the cells are all equal

--- HW4-OO/UnitTestProject1/SudokuReaderTest.cs
+++ HW4-OO/UnitTestProject1/SudokuReaderTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HW4_OO;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
@@ -12,6 +13,32 @@ namespace UnitTestProject1
         {
             SudokuPuzzle Empty = SudokuReader.Read(null);
             Assert.AreEqual(Empty, null);
+        }
+
+        [TestMethod]
+        public void ReadPuzzle1()
+        {
+            List<char> Chars = new List<char> { '1', '2', '3', '4' };
+            string[] puzzleItems = "12-1\n---2\n3-2-\n-4-3".Split();
+            SudokuPuzzle myPuzzle = new SudokuPuzzle(Chars, 4, puzzleItems);
+
+            SudokuPuzzle readPuzzle = SudokuReader.Read("ReadPuzzle1.txt");
+
+            CollectionAssert.AreEqual(myPuzzle.Cells, readPuzzle.Cells);
+            //CollectionAssert.AreEqual(myPuzzle.Cells, readPuzzle.Cells);
+            //CollectionAssert.AreEqual(myPuzzle.Cells, readPuzzle.Cells);
+
+            //Assert.AreEqual(myPuzzle.Rows, 4);
+            //CollectionAssert.AreEqual(myPuzzle.CharacterList, Chars);
+            //Assert.AreEqual(myPuzzle.BlockDimension, 2);
+            //Assert.AreEqual(myPuzzle.Cells[0, 0].Value, '1');
+            //Assert.AreEqual(myPuzzle.Cells[0, 0].Column, 0);
+            //Assert.AreEqual(myPuzzle.Cells[0, 0].Row, 0);
+
+
+
+
+                
         }
     }
 }
