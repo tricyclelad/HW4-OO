7395cb44b3915261cddcb7b23e5b39f48bd45d3b
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-11-19 11:23:53 -0700

    Update SudokuWriter.cs
    Added write method to the sudoku writer. Will need a way to parse the sudoku puzzles to text files. Make a toString method in the sudoku class

--- HW4-OO/HW4-OO/SudokuWriter.cs
+++ HW4-OO/HW4-OO/SudokuWriter.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HW4_OO
 {
-    class SudokuWriter
+    public static class SudokuWriter
     {
+        public static void Write(string path, SudokuPuzzle puzzle)
+        {
+            if (path == null)
+            {
+                Console.WriteLine("Empty Path");
+                return;
+            }
+            if (puzzle == null)
+            {
+                Console.WriteLine("Empty Sudoku Puzzle");
+                return;
+            }
+
+            File.WriteAllText(path,"myPuzzle");
+        }
+
     }
 }
