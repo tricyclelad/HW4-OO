c3d8540deecc637835c8a247f0ea2566eda6cd8c
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-11-19 19:17:31 -0700

    Pseudo Strategy Solver
    This is where the code for a strategy solver would go. It would be able to react to the board state and call different algorithms at run time. currently just loops through the two solvers. Does not detect an invalid sudoku puzzle.

---
+++ HW4-OO/HW4-OO/StrategySolve.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_OO
+{
+    public static class StrategySolve
+    {
+        public static bool Solve(SudokuPuzzle _Puzzle)
+        {
+            SudokuPuzzle LastIterationPuzzle = _Puzzle;
+            SudokuPuzzle CurrentIterationPuzzle = _Puzzle;
+            CellSolutionAlgorithm solver = new OnlyChoiceAlgorithm();
+            bool solved = solver.IsSolved(CurrentIterationPuzzle);
+            if (solved)
+                return true;
+
+            bool puzzleChanged = true;
+            while(puzzleChanged)
+            {
+                solved = solver.SolvePuzzle(CurrentIterationPuzzle);
+                if (LastIterationPuzzle == CurrentIterationPuzzle)
+                {
+                    puzzleChanged = false;
+                }
+                LastIterationPuzzle = CurrentIterationPuzzle;
+            }
+            solved = solver.IsSolved(CurrentIterationPuzzle);
+            if(solved)
+            {
+                _Puzzle = CurrentIterationPuzzle;
+                return true;
+            }
+            
+            solver = new RecursiveAlgorithm();
+            solved = solver.SolvePuzzle(CurrentIterationPuzzle);
+
+            _Puzzle = CurrentIterationPuzzle;
+
+            return solved;
+        }
+    }
+}
---
+++ HW4-OO/UnitTestProject1/StrategySolveTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW4_OO;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class StrategySolveTest
+    {
+        [TestMethod]
+        public void Solve()
+        {
+            List<char> Chars = new List<char> { '1', '2', '3', '4' };
+            string[] puzzleItems = "42-1\n---2\n3-2-\n-4-3".Split();
+            SudokuPuzzle myPuzzle = new SudokuPuzzle(Chars, 4, puzzleItems);
+
+            bool solved = StrategySolve.Solve(myPuzzle);
+
+            Assert.IsTrue(solved);
+        }
+        //[TestMethod]
+        //public void CantSolve()
+        //{
+        //    List<char> Chars = new List<char> { '1', '2', '3', '4' };
+        //    string[] puzzleItems = "4441\n---2\n3-2-\n-4-3".Split();
+        //    SudokuPuzzle myPuzzle = new SudokuPuzzle(Chars, 4, puzzleItems);
+
+        //    bool solved = StrategySolve.Solve(myPuzzle);
+
+        //    Assert.IsFalse(solved);
+        //}
+    }
+}
