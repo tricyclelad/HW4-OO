c5b33f88cdb5aa964b949e6f61e1db277a01045b
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-11-19 18:20:17 -0700

    Added recursive algorithm to solve sudoku boards
    Added recursiveAlgorithm and RecursiveAlgorithm unit test
    Stable

---
+++ HW4-OO/HW4-OO/RecursiveAlgorithm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_OO
+{
+    public class RecursiveAlgorithm : CellSolutionAlgorithm
+    {
+        public override bool SolveCell(SudokuPuzzle _Puzzle, int _Row, int _Column)
+        {
+            if (_Row == _Puzzle.Rows)
+            {
+                return true;
+            }
+
+            if (_Puzzle.Cells[_Row,_Column].Value!= '-')
+            {
+                if (SolveNext(_Puzzle,_Row,_Column))
+                {
+                    return true;
+                }
+                return false;
+            }
+            foreach (var character in _Puzzle.CharacterList)
+            {
+                if (IsValidMove(_Puzzle, _Row, _Column, character))
+                {
+                    _Puzzle.Cells[_Row, _Column].Value = character;
+                    if (SolveNext(_Puzzle, _Row, _Column))
+                    {
+                        return true;
+                    }
+                }
+            }
+            _Puzzle.Cells[_Row, _Column].Value = '-';
+            return false;
+        }
+        private bool SolveNext(SudokuPuzzle _Puzzle, int _Row, int _Column)
+        {
+            if (_Column == _Puzzle.Rows -1)
+            {
+                if (SolveCell(_Puzzle, _Row+1, 0))
+                {
+                    return true;
+                }
+            }
+            else if(SolveCell(_Puzzle, _Row, _Column + 1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsValidMove(SudokuPuzzle _Puzzle, int _Row, int _Column, char value)
+        {
+            var r = _Puzzle.GetRow(_Row);
+            var c = _Puzzle.GetColumn(_Column);
+            var block = _Puzzle.GetBlock(_Row, _Column);
+
+            //Returns false if the value is found anywhere in the row, column, or block
+            return !(r.Any(cell => cell.Value == value) || c.Any(cell => cell.Value == value) || block.Any(cell => cell.Value == value));
+        }
+    }
+}
---
+++ HW4-OO/UnitTestProject1/RecursiveAlgorithmTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW4_OO;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RecursiveAlgorithmTest
+    {
+        [TestMethod]
+        public void RecursiveAlgorithmTotal()
+        {
+            List<char> Chars = new List<char> { '1', '2', '3', '4' };
+            string[] puzzleItems = "42-1\n---2\n3-2-\n-4-3".Split();
+            SudokuPuzzle myPuzzle = new SudokuPuzzle(Chars, 4, puzzleItems);
+
+            CellSolutionAlgorithm solution = new RecursiveAlgorithm();
+
+            bool solved = solution.SolveCell(myPuzzle, 0, 0);
+            Assert.IsTrue(solved);
+            Assert.IsTrue(solution.IsSolved(myPuzzle));
+        }
+        [TestMethod]
+        public void UnsolvableRecursiveAlgorithmTotal()
+        {
+            List<char> Chars = new List<char> { '1', '2', '3', '4' };
+            string[] puzzleItems = "12-1\n---2\n3-2-\n-4-3".Split();
+            SudokuPuzzle myPuzzle = new SudokuPuzzle(Chars, 4, puzzleItems);
+
+            CellSolutionAlgorithm solution = new RecursiveAlgorithm();
+
+            bool solved = solution.SolveCell(myPuzzle, 0, 0);
+            Assert.IsFalse(solved);
+            Assert.IsFalse(solution.IsSolved(myPuzzle));
+        }
+
+
+    }
+}
